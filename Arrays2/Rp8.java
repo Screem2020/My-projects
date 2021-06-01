69e8b58a6f8587d9a52c0898950f516104ae9df5
email <Churin_s@inbox.ru>
2021-06-01 19:49:31 +0200

    com rp8

--- Arrays2/Rp8.java
+++ Arrays2/Rp8.java
@@ -1,7 +1,29 @@
 package Arrays2;
 
+import java.util.Arrays;
+
 public class Rp8 {
     /**
-     * Hello World Java
+     * в одном массиве увеличить максимальные элементы на их порядковые номера
      */
+    public static void main(String[] args) {
+        int[] arr = {2, 12, 99, 1, 99, 54, 99};
+        int max = arr[0];
+        int indexMax = 0;
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= max) {
+                max = arr[i];
+                index = i;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == max) {
+                indexMax++;
+                max += indexMax;
+                arr[i] = max;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
